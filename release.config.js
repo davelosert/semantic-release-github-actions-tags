d18d5df59bf3fe7b3a3d777045896b56d6051c16
David Losert <davelosert@github.com>
2022-10-07 17:20:20 +0200

    ci: Add release configurations (#2)
    * chore: Changed plugin to run in prepare step
    
    * ci: Adds npm release workflow
    
    * test: Fixes import
    
    * ci: Reduces permissions
    
    * ci: Moves permissions to job level
    
    * chore: Adds missing semantic-release package
    
    * chore: Fixes comment

---
+++ release.config.js
@@ -0,0 +1,14 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+  ]
+};
+
+module.exports = config;
