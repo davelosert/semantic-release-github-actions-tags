01621a4ad9fa4f34f56fbfff4c3a402aa878c320
David Losert <davelosert@github.com>
2024-08-23 17:26:31 +0000

    chore: Removes coverage reporting as it does not work with CJS

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   
   test: {
     coverage: {
-      provider: 'v8',
-      include: ['lib/**/*.js'],
-      exclude: ['lib/**/*.test.js'],
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
     }
