d16208213da950d9c292360755bb059e4ab00157
David Losert <davelosert@github.com>
2024-08-23 07:37:55 +0000

    chore: Updates all dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  
   test: {
     coverage: {
+      provider: 'v8',
+      include: ['lib/**/*.js'],
+      exclude: ['lib/**/*.test.js'],
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
     }
