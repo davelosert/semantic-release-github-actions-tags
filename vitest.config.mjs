4326bf7ceea2ca1cfb2d5f0a8d922900de606930
David Losert <davelosert@github.com>
2024-08-23 17:52:21 +0000

    fix: Converting packages to type: module to fix error with semantic-release/error package being esm only

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,6 +4,7 @@ export default defineConfig({
   
   test: {
     coverage: {
+      include: ['lib/**/*.js'],
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
     }
